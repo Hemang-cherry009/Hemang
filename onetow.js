c36a878430edd97c43e7cf01955a867905e9202b
Hemang Gunwant <hemanggunwant@gmail.com>
2025-07-21 00:52:37 +0530

    done1

--- onetow.js
+++ onetow.js
@@ -4,3 +4,5 @@ console.log("used reset hard and the commit got removed complely");
 console.log("used reset hard and the commit got removed complely");
 console.log("used reset hard and the commit got removed complely");
 console.log("used reset hard and the commit got removed complely");
+console.log("used reset hard and the commit got removed complely");
+console.log("used reset hard and the commit got removed complely");
