e171a48a04ce2328651d4fac534249d936f48fee
Hemang Gunwant <hemanggunwant@gmail.com>
2025-07-21 00:34:54 +0530

    ava

--- onetow.js
+++ onetow.js
@@ -1,3 +1,4 @@
 console.log("used reset hard and the commit got removed complely");
 console.log("used reset hard and the commit got removed complely");
 console.log("used reset hard and the commit got removed complely");
+console.log("used reset hard and the commit got removed complely");
